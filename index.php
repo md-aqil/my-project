a1a386a1d989f33acdcbb7d9ca14a3787a247079
aqil4k <mdaqil4k@gmail.com>
2016-08-03 01:22:09 +0530

    upload first project

---
+++ index.php
@@ -0,0 +1,454 @@
+<?php include "include/header.php" ?>
+<div id="page-down"></div>
+
+	<div class="container">
+		<div class="row">
+			<h1>&nbsp;</h1>
+			<h1 class="title-header">Valuable leaders of Asraha, and there thoughts</h1>
+			<h3 class="text-center font-light">Always do your best. What you plant now, you will harvest later.</h3>
+			<div class="text-center  hidden-sm hidden-xs"><img  src="img/line.png" alt=""></div>
+				<p>&nbsp;</p>
+				<div class="col-md-4">
+					<div class="head-container">
+						<h4 class="head-name">Khurshid Anwar</h4>
+						<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+						<div class="head-poaple-img img-name"></div>
+				  		<div class="blog-content-box">
+						  		 <p class="abh">
+								As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>
+							</div>
+				  		<button class="viewallbtn">View all</button>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="head-container">
+						<h4 class="head-name">Khurshid Anwar</h4>
+						<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+							<div class="head-poaple-img img-name"></div>
+					  		<div class="blog-content-box">
+						  		 <p class="abh">
+								As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>
+							</div>
+				  		<button class="viewallbtn">View all</button>
+
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="head-container">
+						<h4 class="head-name">Khurshid Anwar</h4>
+						<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+							<div class="head-poaple-img img-name"></div>
+							<div class="blog-content-box">
+						  		 <p class="abh">
+								As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>
+							</div>
+				  		<a href="" class="viewallbtn">View all</a>
+
+					</div>
+				</div>
+
+		</div>
+	</div><!-- end container -->
+
+	<h1>&nbsp;</h1>
+	<div class="container-fluid bg-gray ">
+
+			<div class="container ">
+			<!-- <p>	&nbsp;</p> -->
+			<div class="slider-container por">
+			
+			<h2 class="title-header text-center gray-tile">Services Provided</h2>
+
+				<span class="prev prevControl">
+				<i class="glyphicon glyphicon-menu-left"></i>
+				</span>
+				<span class="next nextControl">
+				<i class="glyphicon glyphicon-menu-left"></i>
+				</span>
+
+
+					<div class="cycle-slideshow" 
+					    data-cycle-fx="carousel"
+					    data-cycle-carousel-visible="5"
+					    data-cycle-carousel-fluid="true"
+					    data-cycle-timeout="10000"
+					    data-cycle-prev=".prevControl"
+	    				data-cycle-next=".nextControl"
+	    				data-cycle-slides=".imgborder"
+	    				
+					    >
+					    	
+					  		<div class="imgborder">	
+					  			<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+
+					  		</div>
+					  		<div class="imgborder">	
+					  			<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+
+					  		</div>
+					  		<div class="imgborder">	
+					  			<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+
+					  		</div>
+					  
+					</div>
+				</div>
+		</div>
+
+	</div>
+<div class="container overflow-hidden">
+	<h3>&nbsp;</h3>
+	<h2 class="title-header text-center">Services Provided</h2>
+	<div class="text-center  hidden-sm hidden-xs"><img  src="img/line.png" alt=""></div>
+	<h1>&nbsp;</h1>
+	<div class="service-box">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="wow bounceInLeft" data-wow-delay=".2s">
+					<p class="text-center"><i class="glyphicon glyphicon-music service-icon"></i></p>
+						<h3 class="text-center">lorem</h3>
+					<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam id molestias obcaecati optio. Nesciunt quisquam aperiam numquam earum iusto explicabo quod id iure. Quasi temporibus dolore assumenda magnam, officia?</p>
+								
+				</div>
+			</div>
+			<div class="col-md-4 wow bounceInDown" data-wow-delay=".6s">
+			<p class="text-center"><i class="glyphicon glyphicon-music service-icon"></i></p>
+
+				<h3 class="text-center">lorem</h3>
+				<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam id molestias obcaecati optio. Nesciunt quisquam aperiam numquam earum iusto explicabo quod id iure. Quasi temporibus dolore assumenda magnam, officia?</p>
+			</div>
+			<div class="col-md-4 wow rotate bounceInRight" data-wow-delay=".3s">
+				<p class="text-center"><i class="glyphicon glyphicon-music service-icon"></i></p>
+				<h3 class="text-center">lorem</h3>
+				<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam id molestias obcaecati optio. Nesciunt quisquam aperiam numquam earum iusto explicabo quod id iure. Quasi temporibus dolore assumenda magnam, officia?</p>
+			</div>
+			<div class="col-md-4 wow  fadeInDown" data-wow-delay=".6s">
+				<p class="text-center"><i class="glyphicon glyphicon-music service-icon"></i></p>
+				<h3 class="text-center">lorem</h3>
+				<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam id molestias obcaecati optio. Nesciunt quisquam aperiam numquam earum iusto explicabo quod id iure. Quasi temporibus dolore assumenda magnam, officia?</p>
+			</div>
+			<div class="col-md-4 wow fadeInDown" data-wow-delay=".7s">
+				<p class="text-center"><i class="glyphicon glyphicon-music service-icon"></i></p>
+				<h3 class="text-center">lorem</h3>
+				<p class="text-center">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam id molestias obcaecati optio. Nesciunt quisquam aperiam numquam earum iusto explicabo quod id iure. Quasi temporibus dolore assumenda magnam, officia?</p>
+			</div>
+			<div class="col-md-4 wow fadeInDown" data-wow-delay=".8s">
+				<p class="text-center"><i class="glyphicon glyphicon-music service-icon"></i></p>
+				<h3 class="text-center">lorem</h3>
+				<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam id molestias obcaecati optio. Nesciunt quisquam aperiam numquam earum iusto explicabo quod id iure. Quasi temporibus dolore assumenda magnam, officia?</p>
+			</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</div><!-- container -->
+<hr>
+
+<div class="container-fluid">
+<div class="">
+	<h1 class="title-header">Glarries</h1>
+	<div class="text-center  hidden-sm hidden-xs"><img  src="img/line.png" alt=""></div>
+	<h3>&nbsp;</h3>
+	<main class="main-tab-box">
+  <input id="tab1" type="radio" name="tabs" checked>
+  <label for="tab1"> Personalized Plan</label>
+    
+  <input id="tab2" type="radio" name="tabs">
+  <label for="tab2"> For Corporate</label>
+    
+  <input id="tab3" type="radio" name="tabs">
+  <label for="tab3"> Wellness Package</label>
+    
+  <input id="tab4" type="radio" name="tabs">
+  <label for="tab4"> Drupal</label>
+  <section class="tab-content" id="content1">
+    <div class="row">
+	 <div class="col-md-4">
+	 	<div class="tabs-img">
+	 		<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+	 		<div class="tabs-img-overlay">
+	 			<h3 class="over-title">Lorem ipsum dolor sit amet.</h3>
+	 		</div>	
+	 	</div>
+	 </div>
+   	 <div class="col-md-4">
+	 	<div class="tabs-img">
+	 		<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+	 		<div class="tabs-img-overlay">
+	 			<h3 class="title-header">Lorem ipsum dolor sit amet.</h3>
+	 		</div>	
+	 	</div>
+	 </div>
+	  <div class="col-md-4">
+	 	<div class="tabs-img">
+	 		<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+	 		<div class="tabs-img-overlay">
+	 			<h3 class="title-header">Lorem ipsum dolor sit amet.</h3>
+	 		</div>	
+	 	</div>
+	 </div>
+	 <div class="col-md-4">
+	 	<div class="tabs-img">
+	 		<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+	 		<div class="tabs-img-overlay">
+	 			<h3 class="over-title">Lorem ipsum dolor sit amet.</h3>
+	 		</div>	
+	 	</div>
+	 </div>
+	 <div class="col-md-4">
+	 	<div class="tabs-img">
+	 		<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+	 		<div class="tabs-img-overlay">
+	 			<h3 class="over-title">Lorem ipsum dolor sit amet.</h3>
+	 		</div>	
+	 	</div>
+	 </div>
+	  <div class="col-md-4">
+	 	<div class="tabs-img">
+	 		<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+	 		<div class="tabs-img-overlay">
+	 			<h3 class="over-title">Lorem ipsum dolor sit amet.</h3>
+	 		</div>	
+	 	</div>
+	 </div>
+	 </div>
+  </section>
+    
+  <section class="tab-content" id="content2">
+ 	
+  </section>
+    
+  <section class="tab-content" id="content3">
+  <h1>Lorem ipsum dolor sit.</h1>
+   
+  </section>
+    
+  <section class="tab-content" id="content4">
+  	<h1>Lorem ipsum dolor sit.</h1>
+  
+  </section>
+</main>
+</div>
+</div>
+<br><br>
+<div class="container-fluid bg-gray">
+	<div class="pointer-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<p class="rating-p">533</p>
+				</div>
+				<div class="col-md-3">
+					<p class="rating-p">533</p>
+				</div>	
+				<div class="col-md-3">
+					<p class="rating-p">533</p>
+				</div>	
+				<div class="col-md-3">
+					<p class="rating-p">533</p>
+				</div>		
+					
+			</div>
+		</div>
+	</div>
+</div>
+
+	<div class="container">
+		<div class="apporch-box">
+		<h5>&nbsp;</h5>
+			<h1 class="title-header">Information</h1>
+			<div class="text-center  hidden-sm hidden-xs"><img  src="img/line.png" alt=""></div>
+		<h1>&nbsp;</h1>
+			<div class="content-box">
+<ul class="tabs-item clearfix">
+	<li class="col-md-3 "><a class="color-white tabs-a letter wow bounceInDown" data-wow-delay=".3s" href="#first" data-icon="icon-umbrella"></a><p class="">music icon</p></li>
+	<li class="col-md-3 "><a class="color-white tabs-a letter wow bounceInDown" href="#second" data-wow-delay=".4s" data-icon="icon-camera"></a><p class="">music icon</p></li>
+	<li class="col-md-3 "><a class="color-white tabs-a letter wow bounceInDown" href="#third" data-wow-delay=".5s" data-icon="icon-headphones"></a><p class="">music icon</p></li>
+	<li class="col-md-3 "><a class="color-white tabs-a letter wow bounceInDown" href="#fourth" data-wow-delay=".6s" data-icon="icon-music"></a><p class="">music icon</p></li>
+</ul>
+<div id="first" class="animated">
+	<div class="row">	
+		<div class="col-md-6">
+		<br>	
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+		<div class="col-md-6">
+		<br>	
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+	</div>	
+</div>
+
+<div id="second" class="animated">
+<div class="row">	
+		<div class="col-md-6">
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+		<div class="col-md-6">
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+	</div>	
+</div>
+
+
+<div id="third" class="animated">
+		<div class="col-md-6">
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+		<div class="col-md-6">
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+</div>
+	<div id="fourth" class="animated">
+			<div class="col-md-6">
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+		<div class="col-md-6">
+			<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui deserunt beatae, ut dolor! Nihil, commodi ipsum assumenda vero! Perferendis voluptas magni laboriosam cum aliquam, impedit facere, quae error deserunt nam.</p>
+		</div>
+	</div>
+</div>
+
+		</div>
+	</div>
+<!-- clients -->
+
+
+
+<!-- clients -->
+<div class="container-fluid bg-gray">
+	<div class="row">
+		<div class="pointer-box">
+			<div class="">
+				<div class="cycle-slideshow multipule-img-slider" 
+				    data-cycle-fx="carousel"
+				    data-cycle-timeout="2000"
+				    data-cycle-carousel-visible="4"
+				    data-cycle-carousel-fluid="true"
+				    >
+				   <img class="" src="img/Green-Nature-Trees-l.jpg" alt="">
+				   <img class="" src="img/Green-Nature-Trees-l.jpg" alt="">
+				   <img class="" src="img/Green-Nature-Trees-l.jpg" alt="">
+				   <img class="" src="img/Green-Nature-Trees-l.jpg" alt="">
+				   <img class="" src="img/Green-Nature-Trees-l.jpg" alt="">
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+<div id="page-1"></div>
+<div class="container">
+	<div class="row">
+		<div class="our-blog">
+			<h1 class="mt100 text-center font-light">Hello World</h1>
+			<div class="text-center  hidden-sm hidden-xs"><img  src="img/line.png" alt=""></div>
+			<h1>&nbsp;</h1>
+			<div class="col-md-6 img100">
+				<div class="cycle-slideshow" 
+				    data-cycle-fx=scrollHorz
+				    data-cycle-timeout=2000
+				    >
+				    <!-- empty element for overlay -->
+				    <div class="cycle-overlay"></div>
+
+				    <img src="http://malsup.github.io/images/p1.jpg" 
+				        data-cycle-title="Spring" 
+				        data-cycle-desc="Sonnenberg Gardens">
+				    <img src="http://malsup.github.io/images/p2.jpg" 
+				        data-cycle-title="Redwoods" 
+				        data-cycle-desc="Muir Woods National Monument">
+				    <img src="http://malsup.github.io/images/p3.jpg" 
+				        data-cycle-title="Angel Island" 
+				        data-cycle-desc="San Franscisco Bay">
+				    <img src="http://malsup.github.io/images/p4.jpg" 
+				        data-cycle-title="Raquette Lake" 
+				        data-cycle-desc="Adirondack State Park">
+				</div>
+
+			</div>
+			<div class="col-md-6">
+				<img class="img-responsive" src="img/Green-Nature-Trees-l.jpg" alt="">
+				<h3 class="font-light">Lorem ipsum dolor sit amet.</h3>
+				<p class="fz17 font-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium omnis consectetur corporis delectus accusamus neque.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<br>	
+
+<div class="container-flued bg-gray">
+<br>
+	<div class="container">
+		<div class="row">
+		<h1 class="text-center font-light" id="page-2">Contacts</h1>
+			<div class="text-center  hidden-sm hidden-xs"><img  src="img/line.png" alt=""></div>
+			<h2>&nbsp;</h2>
+			<div class="col-md-4 text-center text-style br">
+				<span class="letter" data-icon="glyphicon glyphicon-earphone"></span>
+				<p>7210562014</p>
+			</div>
+			<div class="col-md-4 text-center text-style br">
+					<span class="letter" data-icon="glyphicon glyphicon-envelope"></span>
+				<p>aqilali381@gmail.com</p>
+			</div>
+			<div class="col-md-4 text-center">
+					<span class="letter" data-icon="glyphicon glyphicon-map-marker"></span>
+				<p>
+					Lorem ipsum <br> dolor sit amet, 
+					PO Box 21132
+					Here Weare St, <br> Melbourne
+					Vivas 2355 Australia	
+				</p>
+			</div>
+		</div>
+	</div>
+	<h3>&nbsp;</h3>
+	<div class="container overflow-hidden">
+		<form action="send-mail.php" class="form-horizontal">
+			<div class="form-group">
+				<div class="col-md-6">
+					<input type="text" name="name" class="form-control" placeholder="Enter your name">
+				</div>
+				<div class="col-md-6">
+					<input type="text" name="email" class="form-control" placeholder="Enter your email">
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-md-12">
+					<textarea name="" name="massege" class="form-control" rows="10" placeholder="Write, what you wan't say to us....."></textarea>
+				</div>
+			</div>
+			
+		<div class="text-right">
+			<button class="btn bgcolor-l-blue color-white">SEND &nbsp;<i class="glyphicon glyphicon-send"></i></button>
+		</div>
+		</form>
+	<h3>&nbsp;</h3>
+	</div>
+</div>
+
+<?php include "include/footer.php" ?>
