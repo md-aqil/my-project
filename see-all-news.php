946a0925a86f517afd10298f6cdbef669477adc2
aqil4k <mdaqil4k@gmail.com>
2016-08-17 16:18:04 +0530

    modfyed news section

--- see-all-news.php
+++ see-all-news.php
@@ -7,7 +7,7 @@
 					<h1>&nbsp;</h1>
 					<h1 class="title-header text-left">Latest News</h1>
 					<h3 class="text-center font-light text-left">Always do your best. What you plant now, you will harvest later.</h3>
-					<hr class="styled">
+					<hr class="styled" style="border-color: #c1c1c1;">
 				</div>
 				<div class="col-md-8">
 					<div class="">
@@ -66,93 +66,145 @@
 <p>&nbsp;</p>
 	<div class="container">
 		<div class="row">
-				<div class="col-md-12">
-					<h1>&nbsp;</h1>
-					<h1 class="title-header text-left">Area News</h1>
-					<h3 class="text-center font-light text-left">Always do your best. What you plant now, you will harvest later.</h3>
-					<hr class="styled">
-				</div>
-						<div class="col-md-6 mt50">
-				<div class="row">
-					<div class="col-md-5">
-							<div class="por">
-					     <div class="news-photos"></div>
-					      		<div class="this-overlay2 newslink"> </div>
-	              				<a href="see-all-news.php" class="big-icon cdb" data-toggle="tooltip" title="" data-original-title="Click to view all!">+</a>
-					     </div>
-					     
-					</div>
-					<div class="col-md-7">
-					<h3 class="news-header"><a href="see-all-news.php">Lorem ipsum dolor sit amet. sit ammet couselustion </a></h3>
-					<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; By: Author</p>
-					<hr class="hr-dashed">
-					<div class="blog-content-box">
-						<p class="abh text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque beatae impedit, placeat commodi dolorem, in, voluptatum nesciunt quasi iusto labore, illo optio perspiciatis ad. Repellat assumenda dolorem commodi voluptatibus odit?</p>
-						</div>
-					</div>
+			<div class="col-md-12">
+			<h1>&nbsp;</h1>
+			<h1 class="title-header text-left">Area News</h1>
+			<h3 class="text-center font-light text-left">Always do your best. What you plant now, you will harvest later.</h3>
+			<hr class="styled" style="border-color: #c1c1c1;">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default panel-blog">
+				<div class="panel-heading"><img class="width100" src="img/Green-Nature-Trees-l.jpg" alt=""></div>
+				  <div class="panel-body">
+					  <div>
+					 	 	<h4 class="">lorem ipsum doller sit ammet</h4>
+					  		<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+					  </div>
+					  <hr class="styled">
+					<p class="">
+						As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+				  </div>
 				</div>
 			</div>
-					<div class="col-md-6 mt50">
-				<div class="row">
-					<div class="col-md-5">
-							<div class="por">
-					     <div class="news-photos"></div>
-					      		<div class="this-overlay2 newslink"> </div>
-	              				<a href="see-all-news.php" class="big-icon cdb" data-toggle="tooltip" title="" data-original-title="Click to view all!">+</a>
-					     </div>
-					     
-					</div>
-					<div class="col-md-7">
-					<h3 class="news-header"><a href="see-all-news.php">Lorem ipsum dolor sit amet. sit ammet couselustion </a></h3>
-					<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; By: Author</p>
-					<hr class="hr-dashed">
-					<div class="blog-content-box">
-						<p class="abh text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque beatae impedit, placeat commodi dolorem, in, voluptatum nesciunt quasi iusto labore, illo optio perspiciatis ad. Repellat assumenda dolorem commodi voluptatibus odit?</p>
-						</div>
-					</div>
+			<div class="col-md-4">
+				<div class="panel panel-default panel-blog">
+				<div class="panel-heading"><img class="width100" src="img/Green-Nature-Trees-l.jpg" alt=""></div>
+				  <div class="panel-body">
+					  <div>
+					 	 	<h4 class="">lorem ipsum doller sit ammet</h4>
+					  		<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+					  </div>
+					  <hr class="styled">
+					<p class="">
+						As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+				  </div>
 				</div>
 			</div>
-					<div class="col-md-6 mt50">
-				<div class="row">
-					<div class="col-md-5">
-							<div class="por">
-					     <div class="news-photos"></div>
-					      		<div class="this-overlay2 newslink"> </div>
-	              				<a href="see-all-news.php" class="big-icon cdb" data-toggle="tooltip" title="" data-original-title="Click to view all!">+</a>
-					     </div>
-					     
-					</div>
-					<div class="col-md-7">
-					<h3 class="news-header"><a href="see-all-news.php">Lorem ipsum dolor sit amet. sit ammet couselustion </a></h3>
-					<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; By: Author</p>
-					<hr class="hr-dashed">
-					<div class="blog-content-box">
-						<p class="abh text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque beatae impedit, placeat commodi dolorem, in, voluptatum nesciunt quasi iusto labore, illo optio perspiciatis ad. Repellat assumenda dolorem commodi voluptatibus odit?</p>
-						</div>
-					</div>
+			<div class="col-md-4">
+				<div class="panel panel-default panel-blog">
+				<div class="panel-heading"><img class="width100" src="img/Green-Nature-Trees-l.jpg" alt=""></div>
+				  <div class="panel-body">
+					  <div>
+					 	 	<h4 class="">lorem ipsum doller sit ammet</h4>
+					  		<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+					  </div>
+					  <hr class="styled">
+					<p class="">
+						As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+				  </div>
 				</div>
 			</div>
-					<div class="col-md-6 mt50">
-				<div class="row">
-					<div class="col-md-5">
-							<div class="por">
-					     <div class="news-photos"></div>
-					      		<div class="this-overlay2 newslink"> </div>
-	              				<a href="see-all-news.php" class="big-icon cdb" data-toggle="tooltip" title="" data-original-title="Click to view all!">+</a>
-					     </div>
-					     
-					</div>
-					<div class="col-md-7">
-					<h3 class="news-header"><a href="see-all-news.php">Lorem ipsum dolor sit amet. sit ammet couselustion </a></h3>
-					<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; By: Author</p>
-					<hr class="hr-dashed">
-					<div class="blog-content-box">
-						<p class="abh text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque beatae impedit, placeat commodi dolorem, in, voluptatum nesciunt quasi iusto labore, illo optio perspiciatis ad. Repellat assumenda dolorem commodi voluptatibus odit?</p>
-						</div>
-					</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default panel-blog">
+				<div class="panel-heading"><img class="width100" src="img/asraha1.jpg" alt=""></div>
+				  <div class="panel-body">
+					  <div>
+					 	 	<h4 class="">lorem ipsum doller sit ammet</h4>
+					  		<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+					  </div>
+					  <hr class="styled">
+					<p class="">
+						As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+				  </div>
 				</div>
 			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default panel-blog">
+				<div class="panel-heading"><img class="width100" src="img/Green-Nature-Trees-l.jpg" alt=""></div>
+				  <div class="panel-body">
+					  <div>
+					 	 	<h4 class="">lorem ipsum doller sit ammet</h4>
+					  		<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+					  </div>
+					  <hr class="styled">
+					<p class="">
+						As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+				  </div>
 				</div>
-	</div>	<h1>&nbsp;</h1>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default panel-blog">
+				<div class="panel-heading"><img class="width100" src="img/Green-Nature-Trees-l.jpg" alt=""></div>
+				  <div class="panel-body">
+					  <div>
+					 	 	<h4 class="">lorem ipsum doller sit ammet</h4>
+					  		<p class="text-muted">July 26, 2016 &nbsp; | &nbsp; Mukhya of Ashraha</p>
+					  </div>
+					  <hr class="styled">
+					<p class="">
+						As a leader, I am tough on myself and I raise the standard for everybody; however, I am very caring because I want people to excel at what they are doing so that they can aspire to be me in the future."
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>	
+	<h1>&nbsp;</h1>
 </div>
 <?php include "include/footer.php" ?>
